1b962177c7cbf03381c178254d18cecd597db2ab
bayu <bayurara555@gmail.com>
2025-02-25 09:23:22 +0700

    comit 4

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,20 @@
 import React from 'react';
+//motion
+import { motion } from 'framer-motion'
+//variants
+import { fadeIn } from '../variants'
 
 const Contact = () => {
-  return <div className='section' id='contact'>Contact</div>;
+  return <section className='section' id='contact'>
+    <div className='container mx-auto'>
+      <div>
+        {/* text */}
+        <div>text</div>
+        {/* form */}
+        <div>text</div>
+      </div>
+    </div>
+  </section>;
 };
 
 export default Contact;
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,8 +1,8 @@
 import React from 'react';
 //motion
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 //variants
-import {fadeIn} from '../variants'
+import { fadeIn } from '../variants'
 //img
 import Img1 from '../assets/portfolio-img1.png'
 //img
@@ -14,7 +14,10 @@ const Work = () => {
   return (
     <div className="section" id="work">
       <div className="container mx-auto">
-        <div>
+        <motion.div  variants={fadeIn("right", 0.3)}
+                    initial="hidden"
+                    whileInView={"show"}
+                    viewport={{ once: false, amount: 0.3 }} className='flex flex-col lg:flex-row gap-x-10'>
           <div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
             {/* text */}
             <div>
@@ -30,21 +33,62 @@ const Work = () => {
               <button className="btn btn-sm">View all Projects</button>
             </div>
             {/* img */}
-            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
-              {/* overlay */}
+            <div  className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              {/* overlay */}   
               <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
               {/* img */}
               <img className='group-hover:scale-125 transition-all duration-500' src={Img1} alt="" />
               {/* pretitle */}
-              <div>UI/UI Design</div>
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient '>
+                UI/UI Design
+                  </span>
+              </div>
               {/* title */}
-              <div>
-                <span>Project Title</span>
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3xl text-white'>Project Title</span>
               </div>
             </div>
           </div>
-          <div>2</div>
-        </div>
+          <motion.div  variants={fadeIn("left", 0.5)}
+                      initial="hidden"
+                      whileInView={"show"}
+                      viewport={{ once: false, amount: 0.3 }} className='flex-1 flex flex-col gap-y-12 lg:gap-y-[70px] '>
+          <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              {/* overlay */}   
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/* img */}
+              <img className='group-hover:scale-125 transition-all duration-500' src={Img2} alt="" />
+              {/* pretitle */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient '>
+                UI/UI Design
+                  </span>
+              </div>
+              {/* title */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3xl text-white'>Project Title</span>
+              </div>
+            </div>
+
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              {/* overlay */}   
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/* img */}
+              <img className='group-hover:scale-125 transition-all duration-500' src={Img3} alt="" />
+              {/* pretitle */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient '>
+                UI/UI Design
+                  </span>
+              </div>
+              {/* title */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3xl text-white'>Project Title</span>
+              </div>
+            </div>
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
